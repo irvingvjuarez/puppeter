7c8d4105578738a7c4286ca0c1e53ad36eadb67b
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-11 11:55:47 -0600

    Navigating throught the page

--- __tests__/browser.test.js
+++ __tests__/browser.test.js
@@ -3,16 +3,18 @@ const puppeteer = require("puppeteer")
 describe("First test suit", () => {
 	it("Opening and closing the browser", async () => {
 		const browser = await puppeteer.launch({
-			headless: false,
-			slowMo: 0,
-			devtools: false,
-			args: ["--window-size=1200,800"],
+			headless: true,
 			defaultViewport: null
 		})
 
 		const page = await browser.newPage()
-		await page.goto("https://platzi.com/home")
+		await page.goto("https://yahoo.com/")
+		await page.waitForSelector("img")
+
+		await page.goto("https://platzi.com")
+		await page.goBack()
+		await page.goForward()
 
 		browser.close()
-	}, 8000)
+	}, 30000)
 })
