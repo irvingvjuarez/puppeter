ffc15416bac202d290d1b6c2933f412042f15685
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-11 18:21:38 -0600

    Filling and sending a whole form

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -2,19 +2,29 @@ const puppeteer = require("puppeteer")
 const { MAX_TIMEOUT } = require("../globals")
 
 describe("Testing inputs", () => {
-	it("Popullating input w/data", async () => {
-		const browser = await puppeteer.launch({
+	let browser, page
+
+	beforeAll(async () => {
+		browser = await puppeteer.launch({
 			defaultViewport: null,
 			headless: false
 		})
 
-		const page = await browser.newPage()
+		page = await browser.newPage()
 		await page.goto("https://devexpress.github.io/testcafe/example")
+	}, MAX_TIMEOUT)
+
+	it("Popullating input w/data", async () => {
 		await page.type("#developer-name", "Irving Juárez")
+	}, MAX_TIMEOUT)
+
+	it("Checking a box check input", async () => {
+		await page.click("#tried-test-cafe")
+		await page.type("#comments", "Well, I think is not that good btw")
+		await page.click("#submit-button")
 
 		await new Promise(r => setTimeout(r, 3000));
 
 		browser.close()
-
-	}, MAX_TIMEOUT)
+	})
 })
