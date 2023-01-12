8dd6d814717b17bd5482aef28c9dae2b1d7aeb6d
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-11 20:41:47 -0600

    Awaiting until viewport size changed

--- __tests__/waiting-types.test.js
+++ __tests__/waiting-types.test.js
@@ -22,6 +22,14 @@ describe("Testing different waiting types", () => {
 		await page.goto("https://demoqa.com/modal-dialogs", { waitUntil: "networkidle0" })
 		await page.click("#showSmallModal")
 
-		await page.waitForSelector(".modal-content", { visible: true })
+		// await page.waitForSelector(".modal-content", { visible: true })
+		await page.waitForFunction(() => {
+			return document.getElementById("example-modal-sizes-title-sm").textContent == "Small Modal"
+		})
+
+		const observeResize = page.waitForFunction("window.innerWidth <= 250")
+		await page.setViewport({ width: 199, height: 500 })
+
+		await observeResize
 	}, MAX_TIMEOUT)
 })
