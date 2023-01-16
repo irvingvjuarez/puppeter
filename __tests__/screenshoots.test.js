516bb38af686263149ac617255b0d51222045785
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-16 15:29:36 -0600

    Many screenshots done

--- __tests__/screenshoots.test.js
+++ __tests__/screenshoots.test.js
@@ -12,11 +12,43 @@ describe("Screenshots", () => {
 		page = await browser.newPage()
 	})
 
-	it("Creating screenshots", async () => {
+	beforeEach(async () => {
 		await page.goto("https://platzi.com")
+	})
+
+	it("Creating screenshots", async () => {
 		await page.screenshot({
 			path: "./screenshots/capture.png",
 			fullPage: true
+		})
+	})
+
+	it("Screenshot in a specific area", async () => {
+		await page.screenshot({
+			path: "./screenshots/capture-area.png",
+			clip: {
+				x: 0,
+				y: 0,
+				width: 250,
+				height: 500
+			}
+		})
+	})
+
+	it("Screenshot with transparent background", async () => {
+		await page.evaluate(() => document.body.style.background = "transparent")
+
+		await page.screenshot({
+			path: "./screenshots/transparent-bg.png",
+			fullPage: true
+		})
+	})
+
+	it.only("Screenshot to a certain element", async () => {
+		const testBtn = await page.waitForSelector("a[href='/forms/profile_test/']")
+
+		await testBtn.screenshot({
+			path: "./screenshots/element.png"
 		})
 	})
 })
