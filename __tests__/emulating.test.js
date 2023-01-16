9b573aea3b7beda2d9ab490bd7c2de824ed93f11
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-16 11:10:21 -0600

    Emulating devices with puppeteer config

--- __tests__/emulating.test.js
+++ __tests__/emulating.test.js
@@ -16,9 +16,9 @@ describe("Emulating devices", () => {
 		await page.goto("https://platzi.com")
 	})
 
-	it.only("Emulating mobile phone", async () => {
+	it("Emulating mobile phone", async () => {
 		await page.emulate({
-			name: "Mobile phone",
+			name: "Mobile device",
 			viewport: {
 				width: 375,
 				height: 667,
@@ -30,6 +30,30 @@ describe("Emulating devices", () => {
 			userAgent: "Mozilla/5.0 (Linux; Android 10; SAMSUNG SM-J600G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/10.1 Chrome/71.0.3578.99 Mobile Safari/537.36"
 		})
 
-		await page.waitForTimeout(3000)
+		await new Promise(r => setTimeout(r, 3000))
+	})
+
+	it("Emulating a dektop device", async () => {
+		await page.setViewport({
+			width: 1440,
+			height: 1024
+		})
+
+		await new Promise(r => setTimeout(r, 3000))
+	})
+
+	it("Emulating tablet", async () => {
+		const tablet = puppeteer.KnownDevices["iPad Pro"]
+		await page.emulate(tablet)
+
+		await new Promise(r => setTimeout(r, 3000))
+	})
+
+	it("Emulating mobile phone w/puppeteer", async () => {
+		const mobile = puppeteer.KnownDevices["iPhone 12 Mini landscape"]
+
+		await page.emulate(mobile)
+
+		await new Promise(r => setTimeout(r, 3000))
 	})
 })
