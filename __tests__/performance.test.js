3d1288437e0c8911e97db86c6a029076efbc6654
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-17 10:31:12 -0600

    Getting the metrics of the page

---
+++ __tests__/performance.test.js
@@ -0,0 +1,30 @@
+const puppeteer = require("puppeteer")
+const { MAX_TIMEOUT } = require("../globals")
+
+describe("Testing app performance", () => {
+	let browser, page
+
+	beforeAll(async () => {
+		browser = await puppeteer.launch({
+			defaultViewport: null,
+			headless: true
+		})
+
+		page = await browser.newPage()
+	})
+
+	beforeEach(async () => {
+		await page.goto("https://platzi.com")
+	}, MAX_TIMEOUT)
+
+	afterAll(async () => {
+		await browser.close()
+	})
+
+	it("Measuring automation performance", async () => {
+		await page.waitForSelector("img")
+		const metrics = await page.metrics()
+
+		console.log(metrics)
+	})
+})
