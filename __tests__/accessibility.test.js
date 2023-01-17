92128493e6a47697644ca8976ae1ec3d3fc8604b
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-16 19:23:14 -0600

    Testing accessibility with a third party library

--- __tests__/accessibility.test.js
+++ __tests__/accessibility.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require("puppeteer")
+const { AxePuppeteer } = require("@axe-core/puppeteer")
+const { MAX_TIMEOUT } = require("../globals")
 
 describe("Testing accessibility", () => {
 	let browser, page
@@ -12,17 +14,27 @@ describe("Testing accessibility", () => {
 		page = await browser.newPage()
 	})
 
-	beforeEach(async () => {
-		await page.goto("https://platzi.com")
-	})
+	// beforeEach(async () => {
+	// 	await page.goto("https://platzi.com")
+	// })
 
 	afterAll(async () => {
 		await browser.close()
 	})
 
 	it("Taking accessibility snapshot", async () => {
+		await page.goto("https://platzi.com")
 		await page.waitForSelector("img")
 		const snapshot = await page.accessibility.snapshot()
 		console.log(snapshot)
 	})
+
+	it.only("Testing accessibility w/axe", async () => {
+		await page.setBypassCSP(true)
+		await page.goto("https://platzi.com")
+		await page.waitForSelector("img")
+
+		const accessibilityReport = await new AxePuppeteer(page).analyze()
+		console.log(accessibilityReport.violations[0])
+	}, MAX_TIMEOUT)
 })
